252586f6b94e8d455718745aeaa3a0cd810c25f6
phillipgarci12 <suppliedsum12@gmail.com>
2023-07-24 11:34:13 -0400

    Changed information in personal Section:

--- src/personalSection.js
+++ src/personalSection.js
@@ -5,13 +5,25 @@ const PersonalSection = () => {
     <div>
       <h2>Personal Information</h2>
       <p>
-        Hello, all! My name is Phillip Garcia, and I am a recent college graduate from Furman University. 
-        During my time Furman, I chose to major in computer science and completed my degree in May 2023.
-        Some important coursework I have taken includes: Data Structures, Advanced Data Structures & Algorithims, Artifical Intelligence, Database Management, Software Engineering, and Data Analytics
-        Throughout my time at Furman I have partcipated in projects, some of which are highlighted here on this website. I also have projects that I am currently working on such as this website. 
-        Additionally, I have internship experience in marketing, internal business & sales, and software development
+        Hello, all! My name is Phillip Garcia, and I am a recent college graduate from Furman University.
+        During my time at Furman, I chose to major in computer science and completed my degree in May 2023. 
+        <br></br>
+        <br></br>
+        Relevant coursework I have taken includes: Data Structures, Advanced Data Structures & Algorithims, Artifical Intelligence, Database Management, Software Engineering, and Data Analytics.  
+        <br></br>
+        Important language experience I have includes: Python, Java, Javascript, React.js, AWS, HTML, CSS, PHP, Tableau, and Trello.  
+        <br></br>
+        <br></br>
+        Throughout my time at Furman I have partcipated in projects, some of which are highlighted here on this website.  I also have projects that I am currently working on such as this website.   
+        Additionally, I have internship experience in marketing, internal business & sales, and   software development.  
+        <br></br>
+        <br></br>
         Outside of tech my interests include: videogames, reading, working out, and cooking. 
+        <br></br>
+        <br></br> 
         My passion is to create and through that passion I want to help others and solve problems. 
+        My current goal currently is for full time employment as software engineer/developer
+         
       </p>
     </div>
   );
