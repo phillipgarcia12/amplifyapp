e70f41dccc4a35fee0133329593587b0d64f29b8
phillipgarci12 <suppliedsum12@gmail.com>
2023-07-24 11:22:29 -0400

    Changed bio in personalSection.js

--- src/personalSection.js
+++ src/personalSection.js
@@ -5,11 +5,13 @@ const PersonalSection = () => {
     <div>
       <h2>Personal Information</h2>
       <p>
-        Hello, potential employers! My name is [Your Name], and I'm passionate about [Your Interests]. 
-        I have [X] years of experience in [Your Field] and have worked on various projects that showcase my skills in [Specific Skills or Technologies].
-        I am a quick learner, a problem solver, and I love taking on new challenges. 
-        I believe my dedication and expertise make me a great fit for [Company Name or Industry].
-        Thank you for visiting my website, and I look forward to connecting with you!
+        Hello, all! My name is Phillip Garcia, and I am a recent college graduate from Furman University. 
+        During my time Furman, I chose to major in computer science and completed my degree in May 2023.
+        Some important coursework I have taken includes: Data Structures, Advanced Data Structures & Algorithims, Artifical Intelligence, Database Management, Software Engineering, and Data Analytics
+        Throughout my time at Furman I have partcipated in projects, some of which are highlighted here on this website. I also have projects that I am currently working on such as this website. 
+        Additionally, I have internship experience in marketing, internal business & sales, and software development
+        Outside of tech my interests include: videogames, reading, working out, and cooking. 
+        My passion is to create and through that passion I want to help others and solve problems. 
       </p>
     </div>
   );
